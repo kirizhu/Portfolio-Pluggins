c8c03450636696ca2878bc5742ccaf29bdba1e87
Eddy Garcia <kiribaty88@gmail.com>
2019-06-01 00:00:14 +0200

    added icons, img, buttons,css file,js script file

---
+++ Portfolio/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  /* getting slides to work with id# */
+  $("#slides").superslides({
+    animation: "fade",
+    play: 4000,
+    pagination: false,
+    
+  });
+});
