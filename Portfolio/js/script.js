a481975c0b816a292166fd0c84adf4a28bfa5b78
Eddy Garcia <kiribaty88@gmail.com>
2019-06-01 11:12:57 +0200

    typed script added, added styling and headers

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  /* getting slides to work with id# */
+  /* getting slides to work with id# using JQuery */
   $("#slides").superslides({
     animation: "fade",
     play: 4000,
     pagination: false,
-    
+  });
+  /* getting Typed to work with class using vanillaJS */
+  var typed = new Typed(".typed", {
+      strings:["Software Engineer.","Android/iOS Developer.","Web Developer."],
+      typeSpeed:70,
+      loop:true,
+      startDelay:1000,
+      showCursor:false
   });
 });
