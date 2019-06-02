7280430619a73f7a84642ffe66da268314fe596e
Eddy Garcia <kiribaty88@gmail.com>
2019-06-02 10:36:41 +0200

    script activate piechart when section is reached

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -32,19 +32,27 @@ $(document).ready(function() {
       }
     }
   });
-  /* getting PieChart to work with class using JQuery */
-  $(".chart").easyPieChart({
-    //your options goes here
-    easing: "easyInOut",
-    barColor: "#fff",
-    trackColor: false,
-    scaleColor: false,
-    lineWidth: 4,
-    size: 152,
-    onStep: function(from, to, percent) {
-      $(this.el)
-        .find(".percent")
-        .text(Math.round(percent));
+  /* getting skillsSection class position from the top and storing it in our variable using JQuery */
+  let skillsTopOffset = $(".skillsSection").offset().top;
+  /* Creating a window scroll event (when window is scrolled run this function) */
+  $(window).scroll(function() {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      /* getting PieChart to work with class using JQuery */
+      $(".chart").easyPieChart({
+        //your options goes here
+        easing: "easyInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find(".percent")
+            .text(Math.round(percent));
+        }
+      });
     }
   });
 });
