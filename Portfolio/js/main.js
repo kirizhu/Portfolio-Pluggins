4e6d578db3db51b37056b2633e6e8e8be06812ae
Eddy Garcia <kiribaty88@gmail.com>
2019-06-03 13:17:06 +0200

    halfway done with portfolio section ep5

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if (!finished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
       let elements = { counter1: 3, counter2: 4, counter3: 5, counter4: 4 };
-      Object.keys(elements).map((key, index) => {
+      Object.keys(elements).map(key => {
         const countUp = new CountUp(key, elements[key]);
         countUp.start();
       });
